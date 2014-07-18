bfadc8bbeaba71ab556b31e49a40c5234366305d
kjb1867 <keithjbaker@gmail.com>
2014-07-17 20:57:33 -0400

    Create status_openvpn.sh

---
+++ scripts/dd-wrt/status_openvpn.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source $(dirname $0)/../../config/wrench.conf
+$(dirname $0)/../lcd/print_message.py "Running Command:" "Restarting VPN"
+wget -q --tries=10 --timeout=20 -O - http://google.com > /dev/null
+if [[ $? -eq 0 ]]; then
+        country=$(whois $(curl ifconfig.me) | awk 'tolower($1) ~ /^country:/ { $
+        $(dirname $0)/../lcd/print_message.py "Status: Online" "Country: $count$
+
+else
+        $(dirname $0)/../lcd/print_message.py "Status: Offline" "No Internet"
+fi
