de8d83b39f515c463de0f90864cbebe60ef4633e
kjb1867 <keithbaker@hotmail.com>
2014-07-28 15:53:33 -0400

    Improve speed of status

--- scripts/dd-wrt/status_openvpn.sh
+++ scripts/dd-wrt/status_openvpn.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 source $(dirname $0)/../../config/wrench.conf
 $(dirname $0)/../lcd/print_message.py "Running Command:" "VPN STATUS"
-wget -q --tries=10 --timeout=20 -O - http://google.com > /dev/null
+wget -q --tries=5 --timeout=20 -O - http://google.com > /dev/null
 if [[ $? -eq 0 ]]; then
-        country=$(whois $(curl ifconfig.me) | awk 'tolower($1) ~ /^country:/ { $
+        country=$(whois $(wget -qO- icanhazip.com) | awk 'tolower($1) ~ /^country:/ { $
         $(dirname $0)/../lcd/print_message.py "Status: Online" "Country: $count$
 
 else
